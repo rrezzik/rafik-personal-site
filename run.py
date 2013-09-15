78039dcd02a25cdb75549d2ecc0572056432f251
Rafik Rezzik <rafik.rezzik@gmail.com>
2013-09-14 21:35:21 -0400

    Using tornado

--- run.py
+++ run.py
@@ -1,3 +1,11 @@
 #!flask/bin/python
+
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
 from app import app
-app.run(debug = True, host='0.0.0.0')
+
+http_server = HTTPServer(WSGIContainer(app))
+http_server.listen(5000)
+IOLoop.instance().start()
+#app.run(debug = True, host='0.0.0.0')
