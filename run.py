572d4720290b874a3ffc2017f6be779eedca6942
Rafik Rezzik <rafik.rezzik@gmail.com>
2015-01-29 00:39:46 -0500

    Update run.py to start tornado server on port passed in by command line

--- run.py
+++ run.py
@@ -3,9 +3,14 @@
 from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
+from tornado.options import define, options
 from app import app
 
+options.parse_command_line()
 http_server = HTTPServer(WSGIContainer(app))
-http_server.listen(5000)
+http_server.listen(options.port)
 IOLoop.instance().start()
+#http_server = HTTPServer(WSGIContainer(app))
+#http_server.listen(5000)
+#IOLoop.instance().start()
 #app.run(debug = True, host='0.0.0.0')
