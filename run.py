d565bf57b342c6ef8798a0e6ae8d8527abb38981
Rafik Rezzik <rafik.rezzik@gmail.com>
2015-01-29 00:43:51 -0500

    Forgot to define port as an option for tornado.options to parse

--- run.py
+++ run.py
@@ -6,11 +6,9 @@ from tornado.ioloop import IOLoop
 from tornado.options import define, options
 from app import app
 
+define("port", default=8002, help="Port to listen on", type=int)
+
 options.parse_command_line()
 http_server = HTTPServer(WSGIContainer(app))
 http_server.listen(options.port)
 IOLoop.instance().start()
-#http_server = HTTPServer(WSGIContainer(app))
-#http_server.listen(5000)
-#IOLoop.instance().start()
-#app.run(debug = True, host='0.0.0.0')
