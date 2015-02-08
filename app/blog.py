92586cc129c819b9ee85abec2eeec5df62ba6b70
Rafik Rezzik <rafik.rezzik@gmail.com>
2015-02-07 20:30:15 -0500

    Sort blog posts by reverse chronological order

--- app/blog.py
+++ app/blog.py
@@ -1,5 +1,6 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask.ext.login import login_user, logout_user, current_user, login_required
+from sqlalchemy import desc
 
 from app import app, db, lm
 from forms import LoginForm
@@ -9,7 +10,7 @@ from config import DISQUS_SHORTNAME
 
 @app.route('/blog')
 def blog_index():
-    posts = Post.query.all()
+    posts = Post.query.order_by(desc(Post.timestamp)).all()
     return render_template('blog_index.html', posts=posts)
 
 @app.route('/blog/<postid>')
