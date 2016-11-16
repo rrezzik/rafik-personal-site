a78f5487ab8bc9062d4534fec1a9d8212001aaec
Rafik Rezzik <rafik.rezzik@gmail.com>
2016-11-16 18:51:12 +0000

    Update run.py to not use tornado webserver

--- run.py
+++ run.py
@@ -1,14 +1,4 @@
-#!flask/bin/python
-
-from tornado.wsgi import WSGIContainer
-from tornado.httpserver import HTTPServer
-from tornado.ioloop import IOLoop
-from tornado.options import define, options
 from app import app
 
-define("port", default=8002, help="Port to listen on", type=int)
-
-options.parse_command_line()
-http_server = HTTPServer(WSGIContainer(app))
-http_server.listen(options.port)
-IOLoop.instance().start()
+if __name__ == '__main__':
+    app.run(threaded=True)
